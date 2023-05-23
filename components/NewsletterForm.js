134a9c81636b2a03d1a034e67339d7f287a9d12f
Sebastian Grant <sebastianlgrant@gmail.com>
2023-05-23 14:55:33 +1000

    test

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -78,7 +78,7 @@ export default NewsletterForm
 export const BlogNewsletterForm = ({ title }) => (
   <div className="flex items-center justify-center">
     <div className="bg-gray-100 p-6 dark:bg-gray-800 sm:px-14 sm:py-8">
-      <NewsletterForm title={title} />
+      <NewsletterForm title={title} />p
     </div>
   </div>
 )
